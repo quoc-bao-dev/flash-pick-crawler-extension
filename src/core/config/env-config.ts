c961841177dd3df0fafc50cce759406b073cb683
Quoc Bao <pythagore1102@gmail.com>
2026-04-30 07:35:57 +0700

    feat: add environment configuration constants for service URLs and API key

--- src/core/config/env-config.ts
+++ src/core/config/env-config.ts
@@ -1,5 +1,5 @@
 export const envConfig = {
-    "ingest-service-url": 'https://192.168.1.16:3009',
+    "ingest-service-url": 'https://insgestion-service.flashpick.vn',
     "controll-service-url": 'http://localhost:3010',
     "api-key": "flash-pick-secret-key"
 }

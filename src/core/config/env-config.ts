072e8ec3d3c65339739e0a7de325780aae0f08df
quoc-bao-dev <quocbaodev1102@gmail.com>
2026-06-15 21:34:40 +0700

    chore: update ingest-service-url to local development endpoint

--- src/core/config/env-config.ts
+++ src/core/config/env-config.ts
@@ -1,5 +1,6 @@
 export const envConfig = {
-    "ingest-service-url": 'https://ingestion-service.flashpick.vn',
+    // "ingest-service-url": 'http://localhost:3001/',
+    "ingest-service-url": 'https://ingestion.flashpick.vn/',
     "controll-service-url": 'http://localhost:3010',
     "api-key": "flash-pick-secret-key"
 }
